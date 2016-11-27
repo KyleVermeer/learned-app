15d483435d55adc8db36f81b0bd6659814ae0966
Kyle Vermeer <kvermeer@box.com>
2016-11-27 00:51:50 -0500

    Added Mocha

---
+++ test/core/nugget_management/crud_nugget_test.js
@@ -0,0 +1,13 @@
+/**
+ * Test file for the NuggetCRUDService
+ * @author kvermeer
+ */
+describe('core/nugget_management/crud_nugget', function() {
+    'use strict';
+
+    var sinon = require('sinon');
+
+    describe('NuggetCRUDService', function() {
+
+    });
+});
